d5715004a5615fdbb9c2b2b80c8e4939f6cd33e0
MarcosSousa10 <marcospegodesosua10@gmail.com>
2023-04-23 10:47:09 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import { theme } from '../../chakra.theme';
+import '../styles/globals.css';
+import {SessionProvider} from 'next-auth/react'
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider>
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+    </SessionProvider>
+  )
+}
+
+export default MyApp
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import GithubProvider from 'next-auth/providers/github'
+import CredentialsProvider from    'next-auth/providers/credentials'
+export default NextAuth({
+    providers:[
+        GithubProvider({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
+        }),
+        CredentialsProvider({
+           name:"NextAuthCredentials",
+           credencials:{},
+            async authorize(credentials) {
+                console.log(credentials)
+                return{
+                    name:"marcos",
+                    email:"marcospegodesousa10@gmail.com",
+                    image:"gsds"
+                };
+            },
+           
+})
+       
+    ],
+    secret: process.env.SECRET,
+})
